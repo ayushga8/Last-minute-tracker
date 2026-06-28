6236544e6bfb8d3a8788bb127ef588d72e4471ef
Ayush Garg <ayush82099@gmail.com>
2026-06-28 09:44:54 +0530

    fix: remove celery import from __init__.py

--- lifesaver/__init__.py
+++ lifesaver/__init__.py
@@ -1,3 +1,4 @@
-from .celery import app as celery_app
-
-__all__ = ('celery_app',)
+# Celery import removed for serverless/Render deployment
+# If using Celery, uncomment below:
+# from .celery import app as celery_app
+# __all__ = ('celery_app',)
